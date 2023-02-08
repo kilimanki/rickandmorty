b5f087d1bc1e7075328c77164a4999a0ae757b8a
kilimanki <95223413+kilimanki@users.noreply.github.com>
2023-02-08 14:53:03 +0000

    Deploying to gh-pages from @ kilimanki/rickandmorty@a0737ddf330a576c01368de8462b3a07d786c1ec 🚀

---
+++ index.79b5ceda.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".open"),t=document.querySelector(".hiden-burger");e.addEventListener("click",(function(){document.querySelector(".nav-bar").classList.toggle("not-hiden"),document.querySelector(".nav-bar").classList.toggle("hiden-burger"),t.classList.toggle("hiden-burger"),t.classList.toggle("open-burger"),e.classList.toggle("open"),e.classList.toggle("not-open"),t.classList.contains("hiden-burger")?document.body.style.overflow="auto":document.body.style.overflow="hidden"}))}();
+//# sourceMappingURL=index.79b5ceda.js.map
